65b95cd0711d7ccec935a8dbc8bf321c8f32f3b4
Mriou56 <margot.riou56@gmail.com>
2023-05-31 16:51:19 +0200

    ajout et modification de fichiers

---
+++ constant.php
@@ -0,0 +1,6 @@
+<?php
+    const DB_USER = 'deezertrentuser';
+    const DB_PWD = 'deezertrentpwd';
+    const DB_NAME = 'deezertrentdb';
+    const DB_SERVER = '10.10.51.84';
+    const DB_PORT = '5432';
--- constent.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // Constantes à modifier
-    const DB_USER = '';
-    const DB_PWD = '';
-    const DB_NAME = ''; //pour Margot
-    const DB_SERVER = '10.10.51.84';
-    const DB_PORT = '5432';
