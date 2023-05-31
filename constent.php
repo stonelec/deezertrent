89d8e68b9089cfeb74a6c6eea43494d1486447fd
Mriou56 <margot.riou56@gmail.com>
2023-05-31 14:16:38 +0200

    ajout fichiers

--- constent.php
+++ constent.php
@@ -1,1 +1,7 @@
 <?php
+    // Constantes à modifier
+    const DB_USER = '';
+    const DB_PWD = '';
+    const DB_NAME = ''; //pour Margot
+    const DB_SERVER = '10.10.51.84';
+    const DB_PORT = '5432';
