52f798a0be49869e3ae87dd8f52ef4eb8847365d
Vincent Rocher <vincent.rocher@isen-ouest.yncrea.fr>
2023-06-01 16:36:01 +0200

    add edit song

---
+++ web/acceuil.js
@@ -0,0 +1,7 @@
+var slider = document.getElementById("volume");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
--- web/acceuil.php
+++ web/acceuil.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title>Titre de la page</title>
     <link href="style.css" rel="stylesheet">
+    <script src="acceuil.js"></script>
 
     <!--    Bootstrap    -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
@@ -25,9 +26,24 @@
         <!--    link----------------------------------------------------------------------------------->
         <div class="links d-flex flex-column justify-content-center" >
             <div class="general d-flex flex-column ">
-                <a href="../test.php"><p class="general-link">Accueil</p></a>
-                <a href="../test.php"><p class="general-link">Recherche</p></a>
-                <a href="../test.php"><p class="general-link">Playlist</p></a>
+                <a href="../test.php">
+                    <div class="general-link d-flex flex-row">
+                        <span class="material-symbols-outlined">home</span>
+                        <p class="general-link-text">Accueil</p>
+                    </div>
+                </a>
+                <a href="../test.php">
+                    <div class="general-link d-flex flex-row">
+                        <span class="material-symbols-outlined">search</span>
+                        <p class="general-link-text">Recherche</p>
+                    </div>
+                </a>
+                <a href="../test.php">
+                    <div class="general-link d-flex flex-row" >
+                        <span class="material-symbols-outlined">format_list_bulleted</span>
+                        <p class="general-link-text" style="margin-bottom: 5px">Playlist</p>
+                    </div>
+                </a>
             </div>
             <div class="playlist d-flex flex-column ">
                 <a href="../test.php"><p class="playlist-link">Favoris</p></a>
@@ -40,7 +56,7 @@
         <!--    Navbar TOP ----------------------------------------------------------------------------------->
         <div class="menu-top d-flex flex-row justify-content-between">
             <div>
-                <a class="nom-page" href="../test.php"><h1>Acceuil</h1></a>
+                <a class="nom-page" href="../test.php"><h1>Accueil</h1></a>
             </div>
             <div>
                 <a class="profil d-flex flex-row" href="../test.php"  style="display:block;width:100%;height:100%;">
@@ -56,28 +72,31 @@
         </div>
         <!--    Sound----------------------------------------------------------------------------------->
         <div class="sound d-flex flex-row">
-            <div class="edit d-flex flex-column">
-                <div class="play d-flex flex-row">
-                    <span class="material-symbols-outlined">shuffle</span>
-                    <span class="material-symbols-outlined">skip_previous</span>
-                    <span class="material-symbols-outlined">play_circle</span>
-                    <span class="material-symbols-outlined">skip_next</span>
-                    <span class="material-symbols-outlined">laps</span>
+            <div class="edit d-flex flex-column align-items-center">
+                    <div class="play d-flex justify-content-around align-items-center">
+                    <span class="material-symbols-outlined play-icon-third">shuffle</span>
+                    <span class="material-symbols-outlined play-icon-second">skip_previous</span>
+                    <span class="material-symbols-outlined play-icon-first">play_circle</span>
+                    <span class="material-symbols-outlined play-icon-second">skip_next</span>
+                    <span class="material-symbols-outlined play-icon-third">laps</span>
                 </div>
-                <div class="progres d-flex flex-row">
-                    <p>progres</p>
-                    <p>progres</p>
+                <div class="progres d-flex flex-row justify-content-around align-items-center">
+                    <div>00:0</div>
+                    <div class="test">
+                        <div class="progress" role="progressbar" aria-label="Basic example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="height: 7px">
+                            <div class="progress-bar bg-danger" style="width: 25%"></div>
+                        </div>
+                    </div>
+                    <div>03:4</div>
                 </div>
             </div>
-            <div class="volume d-flex flex-column">
-                <div class="volume-edit d-flex flex-row align-items-center">
+            <div class="volume d-flex flex-column ">
+                <div class="volume-edit d-flex align-items-center ">
                     <div class="volume-logo">
                         <span class="material-symbols-outlined">volume_up</span>
                     </div>
-                    <div class="volume-bar">
-                        <div class="progress" role="progressbar" aria-valuemin="0" aria-valuemax="100" style="height: 12px">
-                            <div class="progress-bar bg-danger" style="width: 50%"></div>
-                        </div>
+                    <div class="volume-bar align-items-center">
+                        <input type="range" min="0" max="100" value="10" class="slider" id="volume">
                     </div>
                 </div>
             </div>
