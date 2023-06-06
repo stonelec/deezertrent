3c3e98bb77c818f22babe93e27e0bbf67311a138
Mriou56 <margot.riou56@gmail.com>
2023-06-06 11:00:23 +0200

    MAJ pour ajout playlist

--- js/playlists.js
+++ js/playlists.js
@@ -1,34 +1,10 @@
 'use strict';
 
-// From to create a new playlist
-let form = '<div class="modal fade" id="new_playlist_modal" tabindex="-1" style="display: none;" aria-hidden="true">\n' +
-    '            <div class="modal-dialog">\n' +
-    '                <div class="modal-content">\n' +
-    '                    <div class="modal-header">\n' +
-    '                        <h1 class="modal-title fs-5" > Ajouter une nouvelle playlist</h1>\n' +
-    '                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>\n' +
-    '                    </div>\n' +
-    '                    <div class="modal-body">\n' +
-    '                        <form>\n' +
-    '                            <div class="mb-3">\n' +
-    '                                <label for="new_name" class="col-form-label"></label>\n' +
-    '                                <input class="form-control" type="text" id="new_name"/>\n' +
-    '                            </div>\n' +
-    '                        </form>\n' +
-    '                    </div>\n' +
-    '                    <div class="modal-footer">\n' +
-    '                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>\n' +
-    '                        <button type="button" class="btn btn-primary">Ajouter</button>\n' +
-    '                    </div>\n' +
-    '                </div>\n' +
-    '            </div>\n' +
-    '        </div>';
-
 // Frame of a list to show the playlist
 function playlist_list(infos){
-    return    '<ul class="list-infos list-group justify-content-center">\n' +
+    return    '<ul class="list-infos list-group justify-content-center info_playlist">\n' +
         '                            <li class="infos d-flex justify-content-between align-items-center get_playlist" id="'+infos['id_playlist']+'" value="'+infos['id_playlist']+'">\n' +
-        '                                <div class="infos-left-part d-flex align-items-center">\n' +
+        '                                <div class="infos-left-part d-flex align-items-center ">\n' +
         '                                    <div class="d-flex flex-row align-items-center">\n' +
         '                                        <div class="overflow">\n' +
         '                                            <h7 class="infos-left-part" style="font-weight: bolder;">'+infos['nom_playlist']+'</h7>\n' +
@@ -39,11 +15,13 @@ function playlist_list(infos){
         '                                   <div class="overflow">\n'+
         '                                       <h7 class="infos-right-date infos-right-part ">'+infos["date_creation"]+'</h7>\n' +
         '                                   </div>\n'+
-        '                                    <i class="bi bi-trash button button-track infos-right-part delete" id="del'+ infos['id_playlist']+'"></i>\n'+
+        '                                    <i class="bi bi-trash button button-track infos-right-part del_playlist" id="del'+ infos['id_playlist']+'"></i>\n'+
         '                                </div>\n' +
         '                            </li>\n' +
         '                        </ul>'
 }
+
+// Frame of a list to show the track in a playlist
 function playlist_card(playlist){
     return '         <div class="card-playlist d-flex flex-column">\n' +
         '                            <div style="margin-left: auto; margin-right: auto;">\n' +
@@ -74,11 +52,11 @@ function track_list(infos){
         '                                        </div>\n' +
         '                                    </div>\n' +
         '                                </div>\n' +
-        '                                <div class="infos-right d-flex flex-row align-items-center">\n' +
+        '                                <div class="infos-right d-flex flex-row align-items-center"  id="track_"'+infos['id_track']+
         '                                   <div class="overflow">\n'+
-        '                                       <h7 class="infos-right-date infos-right-part ">'+infos["date_ajout"]+'</h7>\n' +
+        '                                       <h7 class="infos-right-date infos-right-part">'+infos["date_ajout"]+'</h7>\n' +
         '                                   </div>\n'+
-        '                                    <i class="bi bi-trash button button-track infos-right-part del"></i>\n'+
+        '                                    <i class="bi bi-trash button button-track infos-right-part del_tarck"></i>\n'+
         '                                    <i class="bi bi-plus-lg button button-track infos-right-part add"></i>\n' +
         '                                    <i class="bi bi-heart button button-track infos-right-part add_fav" id="'+infos['id_track']+'"></i>\n' +
         '                                </div>\n' +
@@ -115,11 +93,52 @@ function track_list_search(infos){
         '                            </li>\n' +
         '                        </ul>'
 
-
 }
+
+function playlistDetail(playlist){
+    console.log('fonction playlist detail');
+    if (playlist.length == 0){
+        console.log('playlist est vide');
+        $('#content').html('<h4>Votre playlist est vide</h4>');
+    }else{
+        $('#content').html('<h2 style="margin: 15px 0">'+playlist[0]['nom_playlist']+'</h2>' +
+            '                   <div class="container">' +
+            '                       <div class="row">' +
+            '                           <div class="col-6">'+
+            '                               <div>' +
+            '                                   <h7>'+playlist[0]['date_creation']+'</h7>' +
+            '                               </div>' +
+            '                                   <div>' +
+            '                                   Résultats'+
+            '                               </div>' +
+            '                            </div>' +
+            '                            <div class="col-6 w-50 infos-right-part d-flex align-self-end ms-auto p-2" style="right: 5em">' +
+            '                               <i class="bi bi-sort-alpha-down infos-right-part button-track" id="sort_playlist"></i>' +
+            '                            </div>'+
+            '                           </div >' +
+            '                       </div>'+
+            '                       <div id="liste">' +
+            '                       </div>'+
+            '                   </div>');
+
+        for(let info of playlist) {
+            $('#liste').append(track_list(info))
+        }
+    }
+}
+
+// Function to open a window to ask the name of a new playlist
+function open_modal(){
+    console.log('try to open');
+    let new_modal = document.getElementById("new_modal");
+    new_modal.style.visibility = new_modal.style.visibility === "visible" ? "hidden" : "visible";
+}
+
+// Ajax request
 
 ///////////////////   DEFAULT REQUEST   ///////////////////////////
 // ajaxRequest('GET', '../php/request.php/playlist/', displayListePlaylist);
+
 
 
 
@@ -140,85 +159,41 @@ function displayListePlaylist(playlists) {
         $('#title-page').append('<input type="text" class="bar" id="bar" name="bar" placeholder="&#61442; Recherche">')
         $('#content').html('<h2 style="margin: 15px 0;">Vos playlist</h2>\n');
         $('#content').append('<div class="d-flex flex-row-reverse" style="padding-right: 10%; font-size: 2rem;  height: 4rem">\n' +
-            '                        <i class="bi bi-sort-alpha-down button button_sort"></i>\n' +
-            '                        <i class="bi bi-file-plus button button_add_playlist"></i>\n' +
+            '                        <i class="bi bi-sort-alpha-down button button_sort" ></i>\n' +
+            '                        <i class="bi bi-file-plus button button_add_playlist" onclick="open_modal()"></i>\n' +
             '\n' +
             '                    </div>');
         for (let playlist of playlists)
             $('#content').append(playlist_list(playlist));
 
-    }
-///////////////    CREATE A NEW PLAYLIST    ///////////////////
-$('#add_playlist').click((event) =>
-        {
-            console.log('LE TEST DE NEW');
-            modal();
-            //$('#content').append(form);
-            ajaxRequest('POST', '../php/request.php/playlist/', () =>
+
+        ///////////////    CREATE A NEW PLAYLIST    ///////////////////
+        /*$('#add_playlist').click((event) =>
             {
-                ajaxRequest('GET', '../php/request.php/playlist/', displayListePlaylist);
-            },'nom=' + modalBodyInput , $('#new_playlist').val() );
-        }
-    );
-}
-
-function modal(){
-    const new_modal = document.getElementById('new_playlist_modal')
-    if (new_playlist_modal) {
-        new_playlist_modal.addEventListener('show.bs.modal', event => {
-            // Button that triggered the modal
-            const button = event.relatedTarget
-            // Extract info from data-bs-* attributes
-            const recipient = button.getAttribute('data-bs-whatever')
-            // If necessary, you could initiate an Ajax request here
-            // and then do the updating in a callback.
-
-            // Update the modal's content.
-            const modalBodyInput = new_playlist_modal.querySelector('.modal-body input')
-            const new_name = $('new_name').val()
-        })
+                console.log('LE TEST DE NEW');
+                //$('#content').append(form);
+                ajaxRequest('POST', '../php/request.php/playlist/', () =>
+                {
+                    ajaxRequest('GET', '../php/request.php/playlist/', displayListePlaylist);
+                },'nom=' + modal_body.value , $('#new_playlist').val() );
+            }
+        );*/
     }
 }
 
+//////////////     INFORMATION ABOUT A PLAYLIST     //////////////
 $(document).on('click', '.get_playlist', function (event){
     var id_playlist = $(this).attr('id');
     ajaxRequest('GET', '../php/request.php/playlist/?id_playlist='+ id_playlist, playlistDetail)
 });
 
 
-
 ///////////////    DELETE A PLAYLIST    ///////////////////
-$('#content').on('click', '.del', ()=>{
+// PB effectue la fonction affiche détaill en mme temps et ne supprime pas
+$(document).on('click', '.del_playlist', ()=>{
+    console.log("delete");
+    console.log($(event.target).parent().parent().attr('id'));
     ajaxRequest('DELETE', '../php/request.php/playlist/' +'?id=' + $(event.target).parent().parent().attr('id') , () => {
         ajaxRequest('GET', '../php/request.php/playlist/', displayListePlaylist);
         })
-});
-
-function playlistDetail(playlist){
-    console.log('fonction playlist detail');
-    $('#content').html('<h2 style="margin: 15px 0">'+playlist[0]['nom_playlist']+'</h2>' +
-    '                   <div class="container">' +
-    '                       <div class="row">' +
-    '                           <div class="col-6">'+
-    '                               <div>' +
-    '                                   <h7>'+playlist[0]['date_creation']+'</h7>' +
-    '                               </div>' +
-    '                                   <div>' +
-    '                                   Résultats'+
-    '                               </div>' +
-    '                            </div>' +
-    '                            <div class="col-6 w-50 infos-right-part d-flex align-self-end ms-auto p-2" style="right: 5em">' +
-    '                               <i class="bi bi-sort-alpha-down infos-right-part button-track id=del_playlist"></i>' +
-    '                            </div>'+
-    '                           </div >' +
-    '                       </div>'+
-    '                       <div id="liste">' +
-    '                       </div>'+
-    '                   </div>');
-
-    for(let info of playlist) {
-        $('#liste').append(track_list(info))
-    }
-}
-
-//////////////////////////    DIPSLAY HISTORIQUE    //////////////////////////////
+})
--- php/test_modal.php
+++ php/test_modal.php
@@ -1,89 +1,77 @@
 <!DOCTYPE html>
 <html lang="fr">
+
     <head>
         <meta charset="utf-8">
-        <title>deezertrent</title>
-        <link href="style.css" rel="stylesheet">
+        <title>LE test </title>
+        <link href="../web/style.css" rel="stylesheet">
 
-
-        <!--    Bootstrap    -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-        <!--    Icons Bootstrap-->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-        <!--    Icons google-->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-
-        <!-- JS Script -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"
-                integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=" crossorigin="anonymous" defer></script>
-        <script src="../js/accueil.js" defer></script>
-        <script src="../js/ajax.js" defer></script>
-        <script src="../js/window.js" defer></script>
-        <script src="../js/progresBar.js" defer></script>
-        <script src="../js/playlists.js" defer></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"
-                integrity="sha256-x3YZWtRjM8bJqf48dFAv/qmgL68SI4jqNWeSLMZaMGA=" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js"
-                integrity="sha256-WqU1JavFxSAMcLP2WIOI+GB2zWmShMI82mTpLDcqFUg=" crossorigin="anonymous"></script>
-
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-        <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
-        <title>Bootstrap Example</title>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-
+        <style>
+            body {
+                height: 100%;
+                margin: 0;
+                padding: 0;
+                font-family: Arial, Helvetica, sans-serif;
+            }
+            h2 {
+                font-size: 20px;
+                color: #000000;
+            }
+            #example {
+                visibility: hidden;
+                position: absolute;
+                left: 0;
+                top: 0;
+                width: 100%;
+                height: 100%;
+                text-align: center;
+                z-index: 1000;
+            }
+            #new_modal div {
+                width: 350px;
+                height: 80px;
+                margin: 100px auto;
+                background-color: #f2f2f2;
+                border-radius: 10px;
+                -webkit-border-radius: 10px;
+                -moz-border-radius: 10px;
+                padding: 15px;
+                text-align: center;
+                font-weight: bold;
+                font-size: 15px;
+                border: 3px solid #cccccc;
+                position: absolute;
+                left: 50%;
+                top: 100px;
+                transform: translate(-50%, -50%);
+                -ms-transform: translate(-50%, -50%);
+                -webkit-transform: translate(-50%, -50%);
+            }
+        </style>
     </head>
     <body>
-        <h1>TEST</h1>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tempus tortor id suscipit porttitor. Proin non velit a magna tempor tincidunt aliquam vel libero. Vestibulum vitae erat est. Nam hendrerit massa non mi condimentum, ac condimentum nulla aliquet. Donec a sagittis orci. Aliquam viverra quam id ipsum accumsan hendrerit. Aenean vehicula commodo velit a gravida. Praesent aliquam mattis arcu, imperdiet consectetur diam rutrum vel. Phasellus ultrices sit amet ligula eu mattis. Sed sagittis tellus id enim auctor, in rhoncus massa volutpat.
-        </p>
-
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#new_playlist_modal" data-bs-whatever="@fat">New</button>
-
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tempus tortor id suscipit porttitor. Proin non velit a magna tempor tincidunt aliquam vel libero. Vestibulum vitae erat est. Nam hendrerit massa non mi condimentum, ac condimentum nulla aliquet. Donec a sagittis orci. Aliquam viverra quam id ipsum accumsan hendrerit. Aenean vehicula commodo velit a gravida. Praesent aliquam mattis arcu, imperdiet consectetur diam rutrum vel. Phasellus ultrices sit amet ligula eu mattis. Sed sagittis tellus id enim auctor, in rhoncus massa volutpat.
-        </p>
-
-        <div class="modal fade" id="new_playlist_modal" tabindex="-1" style="display: none;" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" > Ajouter une nouvelle playlist</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <form>
-                            <div class="mb-3">
-                                <label for="new_name" class="col-form-label"></label>
-                                <input class="form-control" type="text" id="new_name"/>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
-                        <button type="button" class="btn btn-primary">Ajouter</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    <script>
-        const new_modal = document.getElementById('new_playlist_modal')
-        if (new_playlist_modal) {
-            new_playlist_modal.addEventListener('show.bs.modal', event => {
-                // Button that triggered the modal
-                const button = event.relatedTarget
-                // Extract info from data-bs-* attributes
-                const recipient = button.getAttribute('data-bs-whatever')
-                // If necessary, you could initiate an Ajax request here
-                // and then do the updating in a callback.
-
-                // Update the modal's content.
-                const modalBodyInput = new_playlist_modal.querySelector('.modal-body input')
-                const new_name = $('new_name').val()
-            })
-        }
-    </script>
+    <i class="bi bi-file-plus infos-reght-part button-track"  style="size: 50px" onclick="open_modal()"> EL BUTTON</i>
+    <div id="new_modal">
+                  <div id="modal_header">
+                       <h2>Ajouter une nouvelle playlist :</h2>
+                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                     </div>
+                  <div id="modal_body">
+                            <label for="new_name">Test:</label>
+                           <input class="form-control" type="text" id="new_name"/>
+                  </div>
+                  <div id="modal_footer">
+                           <a href="#" onclick="open_modal()">Annuler</a>
+                           <button type="button" class="btn btn-primary">Ajouter</button>
+                       </div>
+    </div>
+        <script>
+            function open_modal() {
+                mew = document.getElementById("new_modal");
+                mew.style.visibility = mew.style.visibility == "visible" ? "hidden" : "visible";
+            }
+        </script>
+    </div>
     </body>
 </html>
 
